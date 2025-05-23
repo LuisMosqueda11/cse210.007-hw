0ed1c95d0cb0cfdf717d1cb0e8dce3c5d0a8d4c6
Luis Mosqueda <mos24031@byui.edu>
2025-05-23 13:28:47 -0700

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        string letter;
+        string sign = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter != "F") // No + or - for F grades
+        {
+            int lastDigit = gradePercentage % 10;
+            
+            if (lastDigit >= 7 && letter != "A") // No A+
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3 && gradePercentage != 100) // No A- for 100%
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! You'll do better next time.");
+        }
     }
 }
